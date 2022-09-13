8d39cc8e48a530e96678c2782ec35a158beb1fc7
Lob1t0 <110936797+Lob1t0@users.noreply.github.com>
2022-09-13 12:32:07 -0500

    Update calcu.py

--- calcu.py
+++ calcu.py
@@ -33,7 +33,7 @@ alto_boton=2
 
 display=StringVar()
 
-pantalla_p=Entry(ventana, textvariable=display,fg="black",bg="silver",font=("arial",20,"bold"),width=22,bd=20,justify="right").place(x=37,y=50)
+pantalla_p=Entry(ventana, textvariable=display,fg="black",bg="silver",font=("arial",20,"bold"),width=22,bd=20,justify="right",relief=RIDGE).place(x=37,y=50)
 operador=""
 
 
@@ -63,70 +63,70 @@ def kelvin_farenheit():
                 Resultado_grados.set(respuesta)
 
 
-cen__faren=Button(ventana,text="° C--> °F",bg=color_boton,command=centigrados_farenheit,width=20).place(x=550,y=180)
-faren__cen=Button(ventana,text="°F ---> °C",bg=color_boton,command=farenheit_centigrados,width=20).place(x=550,y=230)
-cen__kel=Button(ventana,text="°C ---> °K",bg=color_boton,command=centigrados_kelvin,width=20).place(x=550,y=280)
-kel__cen=Button(ventana,text="°K ---> °C",bg=color_boton,command=kelvin_centigrados,width=20).place(x=550,y=330)
-faren__kel=Button(ventana,text="°F ---> °K",bg=color_boton,command=farentheit_kelvin,width=20).place(x=550,y=380)
-kel__faren=Button(ventana,text="°K ---> °F",bg=color_boton,command=kelvin_farenheit,width=20).place(x=550,y=430)
+cen__faren=Button(ventana,text="° C--> °F",bg=color_boton,command=centigrados_farenheit,width=20,relief=RIDGE).place(x=550,y=180)
+faren__cen=Button(ventana,text="°F ---> °C",bg=color_boton,command=farenheit_centigrados,width=20,relief=RIDGE).place(x=550,y=230)
+cen__kel=Button(ventana,text="°C ---> °K",bg=color_boton,command=centigrados_kelvin,width=20,relief=RIDGE).place(x=550,y=280)
+kel__cen=Button(ventana,text="°K ---> °C",bg=color_boton,command=kelvin_centigrados,width=20,relief=RIDGE).place(x=550,y=330)
+faren__kel=Button(ventana,text="°F ---> °K",bg=color_boton,command=farentheit_kelvin,width=20,relief=RIDGE).place(x=550,y=380)
+kel__faren=Button(ventana,text="°K ---> °F",bg=color_boton,command=kelvin_farenheit,width=20,relief=RIDGE).place(x=550,y=430)
 
 
 grado=StringVar()
-pantalla1=Entry(ventana,bg="silver",font=("arial",20,"bold"),width=10,bd=20,justify="right",textvariable=grado).place(x=420,y=50)
+pantalla1=Entry(ventana,fg="black",bg="silver",font=("arial",20,"bold"),width=10,bd=20,justify="right",textvariable=grado,relief=RIDGE).place(x=420,y=50)
 
 
 
 Resultado_grados=StringVar()
-pantalla_resultado=Entry(ventana,fg="black",bg="silver",font=("arial",20,"bold"),width=11,bd=20,justify="right",textvariable=Resultado_grados).place(x=620,y=50)
+pantalla_resultado=Entry(ventana,fg="black",bg="silver",font=("arial",20,"bold"),width=11,bd=20,justify="right",textvariable=Resultado_grados,relief=RIDGE).place(x=620,y=50)
 
 
-B0=Button(ventana,text="0",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(0)).place(x=17,y=180)
-B1=Button(ventana,text="1",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(1)).place(x=63,y=180)
-B2=Button(ventana,text="2",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(2)).place(x=109,y=180)
-B3=Button(ventana,text="3",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(3)).place(x=155,y=180)
-B4=Button(ventana,text="4",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(4)).place(x=17,y=230)
-B5=Button(ventana,text="5",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(5)).place(x=63,y=230)
-B6=Button(ventana,text="6",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(6)).place(x=109,y=230)
-B7=Button(ventana,text="7",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(7)).place(x=155,y=230)
-B8=Button(ventana,text="8",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(8)).place(x=17,y=280)
-B9=Button(ventana,text="9",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(9)).place(x=63,y=280)
+B0=Button(ventana,text="0",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(0),relief=RIDGE).place(x=17,y=180)
+B1=Button(ventana,text="1",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(1),relief=RIDGE).place(x=63,y=180)
+B2=Button(ventana,text="2",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(2),relief=RIDGE).place(x=109,y=180)
+B3=Button(ventana,text="3",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(3),relief=RIDGE).place(x=155,y=180)
+B4=Button(ventana,text="4",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(4),relief=RIDGE).place(x=17,y=230)
+B5=Button(ventana,text="5",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(5),relief=RIDGE).place(x=63,y=230)
+B6=Button(ventana,text="6",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(6),relief=RIDGE).place(x=109,y=230)
+B7=Button(ventana,text="7",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(7),relief=RIDGE).place(x=155,y=230)
+B8=Button(ventana,text="8",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(8),relief=RIDGE).place(x=17,y=280)
+B9=Button(ventana,text="9",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(9),relief=RIDGE).place(x=63,y=280)
 
 
-BC=Button(ventana,text="π",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("pi")).place(x=109,y=280)
-BComa=Button(ventana,text=",",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(".")).place(x=155,y=280)
-BotonSuma=Button(ventana,text="+",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("+")).place(x=17,y=330)
-BResta=Button(ventana,text="-",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("-")).place(x=63,y=330)
-BMulti=Button(ventana,text="*",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("*")).place(x=109,y=330)
-BDiv=Button(ventana,text="/",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("/")).place(x=155,y=330)
-BSqrt=Button(ventana,text="√",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("sqrt")).place(x=17,y=380)
-BParen1=Button(ventana,text="(",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("(")).place(x=63,y=380)
-BParen2=Button(ventana,text=")",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(")")).place(x=109,y=380)
-BResto=Button(ventana,text="%",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("%")).place(x=155,y=380)
-Bln=Button(ventana,text="ln",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("log")).place(x=17,y=430)
-BC=Button(ventana,text="C",bg=color_boton,width=ancho_boton,height=alto_boton,command=clear).place(x=63,y=430)
-BExp=Button(ventana,text="EXP",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("**")).place(x=109,y=430)
-BResul=Button(ventana,text="=",bg=color_boton,width=ancho_boton,height=alto_boton,command=operacion).place(x=155,y=430)
-bsen=Button(ventana,text="n!",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("factorial")).place(x=220,y=180)
-bcos=Button(ventana,text="ABS",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("fabs")).place(x=266,y=180)
+BC=Button(ventana,text="π",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("pi"),relief=RIDGE).place(x=109,y=280)
+BComa=Button(ventana,text=",",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("."),relief=RIDGE).place(x=155,y=280)
+BotonSuma=Button(ventana,text="+",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("+"),relief=RIDGE).place(x=17,y=330)
+BResta=Button(ventana,text="-",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("-"),relief=RIDGE).place(x=63,y=330)
+BMulti=Button(ventana,text="*",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("*"),relief=RIDGE).place(x=109,y=330)
+BDiv=Button(ventana,text="/",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("/"),relief=RIDGE).place(x=155,y=330)
+BSqrt=Button(ventana,text="√",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("sqrt"),relief=RIDGE).place(x=17,y=380)
+BParen1=Button(ventana,text="(",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("("),relief=RIDGE).place(x=63,y=380)
+BParen2=Button(ventana,text=")",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik(")"),relief=RIDGE).place(x=109,y=380)
+BResto=Button(ventana,text="%",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("%"),relief=RIDGE).place(x=155,y=380)
+Bln=Button(ventana,text="ln",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("log"),relief=RIDGE).place(x=17,y=430)
+BC=Button(ventana,text="C",bg=color_boton,width=ancho_boton,height=alto_boton,command=clear,relief=RIDGE).place(x=63,y=430)
+BExp=Button(ventana,text="EXP",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("**"),relief=RIDGE).place(x=109,y=430)
+BResul=Button(ventana,text="=",bg=color_boton,width=ancho_boton,height=alto_boton,command=operacion,relief=RIDGE).place(x=155,y=430)
+bsen=Button(ventana,text="n!",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("factorial"),relief=RIDGE).place(x=220,y=180)
+bcos=Button(ventana,text="ABS",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("fabs"),relief=RIDGE).place(x=266,y=180)
 
 
-bsen=Button(ventana,text="sin",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("sin")).place(x=312,y=180)
-bcos=Button(ventana,text="cos",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("cos")).place(x=358,y=180)
-btan=Button(ventana,text="tan",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("tan")).place(x=220,y=230)
-barccot=Button(ventana,text="arccot",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("atan")).place(x=266,y=230)
-barcsen=Button(ventana,text="arccos",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("acos")).place(x=312,y=230)
-barcc=Button(ventana,text="arcsen",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("asin")).place(x=358,y=230)
-bsenh=Button(ventana,text="senh",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("sinh")).place(x=220,y=280)
-bcosh=Button(ventana,text="cosh",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("cosh")).place(x=266,y=280)
-btanh=Button(ventana,text="tanh",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("tanh")).place(x=312,y=280)
-b_Salir=Button(ventana,text="EXIT",bg="yellow",fg="blue",command=exit,font=("Arial Black",5),width=20,relief=RIDGE).place(x=310,y=440)
+bsen=Button(ventana,text="sin",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("sin"),relief=RIDGE).place(x=312,y=180)
+bcos=Button(ventana,text="cos",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("cos"),relief=RIDGE).place(x=358,y=180)
+btan=Button(ventana,text="tan",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("tan"),relief=RIDGE).place(x=220,y=230)
+barccot=Button(ventana,text="arccot",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("atan"),relief=RIDGE).place(x=266,y=230)
+barcsen=Button(ventana,text="arccos",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("acos"),relief=RIDGE).place(x=312,y=230)
+barcc=Button(ventana,text="arcsen",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("asin"),relief=RIDGE).place(x=358,y=230)
+bsenh=Button(ventana,text="senh",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("sinh"),relief=RIDGE).place(x=220,y=280)
+bcosh=Button(ventana,text="cosh",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("cosh"),relief=RIDGE).place(x=266,y=280)
+btanh=Button(ventana,text="tanh",bg=color_boton,width=ancho_boton,height=alto_boton,command=lambda:btnClik("tanh"),relief=RIDGE).place(x=312,y=280)
+b_Salir=Button(ventana,text="EXIT",bg="silver",fg="black",command=exit,font=("Arial Black",5),width=20,relief=RIDGE).place(x=310,y=440)
 
  
-etiqueta=Label(ventana,text="DISPLAY",fg="black",font=("Arial Black",10)).place(x=200,y=29)
-etiqueta2=Label(ventana,text="Ingrese datos por teclado",fg="black",font=("Arial Black",10)).place(x=420,y=25)
-etiqueta3=Label(ventana,text="Resultado",fg="black",font=("Arial Black",10)).place(x=670,y=25)
-etiqueta4=Label(ventana,text="OPERACIONES ARITMETICAS Y GEOMETRICAS",fg="black",font=("Arial Black",10)).place(x=43,y=150)
-etiqueta5=Label(ventana,text="CONVERSION DE TEMPERATURA",fg="black",font=("Arial Black",10)).place(x=500,y=150)
+etiqueta=Label(ventana,text="DISPLAY",bg="silver",fg="black",font=("Arial Black",10)).place(x=200,y=29)
+etiqueta2=Label(ventana,text="Ingrese datos por teclado",bg="silver",fg="black",font=("Arial Black",10)).place(x=420,y=25)
+etiqueta3=Label(ventana,text="Resultado",bg="silver",fg="black",font=("Arial Black",10)).place(x=670,y=25)
+etiqueta4=Label(ventana,text="OPERACIONES ARITMETICAS Y GEOMETRICAS",bg="silver",fg="black",font=("Arial Black",10)).place(x=43,y=150)
+etiqueta5=Label(ventana,text="CONVERSION DE TEMPERATURA",bg="silver",fg="black",font=("Arial Black",10)).place(x=500,y=150)
 
 
 
